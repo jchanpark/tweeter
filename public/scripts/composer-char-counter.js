d8fe523a9e194295c5c4782553f8f3e33f4f3343
jchanpark <junpark4936@gamil.com>
2022-03-30 15:45:14 -0400

    character counter function

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // console.log(this);
+  let len = 140;
+  let remainingCharacter;
+  $('#tweet-text').on('input', function() {
+    let count = $(this).val().length;
+    remainingCharacter = len - count;
+    $('.counter').text(remainingCharacter);
+    if (remainingCharacter < 0) {
+      $('.counter').css("color", "red");
+    } else {
+      $('.counter').css("color", "");
+    }
+  })
+
+});
+
