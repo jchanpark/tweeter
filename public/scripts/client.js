88736480cbcef78eda5271d36c03c4f0ad7199e3
jchanpark <junpark4936@gamil.com>
2022-04-01 10:24:17 -0400

    Jquery post request has a problem

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,38 +4,17 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 const createTweetElement = function(tweet) {
   const { user, content, created_at } = tweet;
+  // console.log('user[0].avatars', user[0].avatars);
+  console.log('user.name', user.name);
   const $tweet = $(
     `<article>
     <header class="user">
       <div class="icon">
-        <img class="userFace" src=${user.avatars}></img><span>${user.name}</span>
+        <img class="userFace" src=${user[0].avatars}></img><span>${user.name}</span>
       </div>
       <div class="email"><span>${user.handle}</span>
       </div>
@@ -61,14 +40,37 @@ const renderTweets = function(object) {
   return;
 }
 $(document).ready(function() {
-  renderTweets(data);
-
+  
   $('#tweets').submit(function(event) {
     event.preventDefault();
-    console.log('data', data);
-    console.log('this.serialize()',$(this).serialize());
-    $.post('/tweets', $(this).serialize());
+    
+    if (!document.getElementById("tweet-text").value) {
+      console.log('document.getElementById("tweet-text").value', document.getElementById("tweet-text").value);
+      alert("Please enter a message");
+      return;
+    }
+    if (document.getElementById("tweet-text").value.length > 140) {
+      alert("Please enter a message of less than 140 characters");
+      return;
+    }
+
+    $.post('/tweets', $(this).serialize()).then( data => {
+      console.log('data', data);
+      createTweetElement(data);
+      
+    })
+  
+    
   });
+
+  const loadTweets = function() {
+    $.get("http://localhost:8080/tweets").then( data => {
+      // console.log('data', data);
+      renderTweets(data);
+    })
+  }
+  loadTweets();
+
 })
 
 
